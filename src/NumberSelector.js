5c8a9c4bde095f48ba3c86539345e4336796e2c6
Alexander Schad <ags@vanguardistas.net>
2021-01-18 14:03:26 +0100

    show all numbers in the selector

--- src/NumberSelector.js
+++ src/NumberSelector.js
@@ -1,14 +1,7 @@
 import React from 'react'
-import { rows, cols, squares } from './data';
 
-// Popup to select a number for a field. shows only the available numbers
-export default function NumberSelector({clicked, setClicked, selectNumber, selPos, fields}) {
-  const row = rows.find(r => r.includes(clicked));
-  const col = cols.find(c => c.includes(clicked));
-  const square = squares.find(s => s.includes(clicked));
-  const usedIndices = (row && col && square) ? row.concat(col).concat(square) : [];
-  const usedVals = usedIndices.map(ui => fields[ui]).filter(i => i !== 0 && i !== fields[clicked]);
-
+// Popup to select a number for a field.
+export default function NumberSelector({clicked, setClicked, selectNumber, selPos}) {
   const nCont = document.getElementById('numberSelectorContainer');
   const height = nCont ? Math.floor(nCont.offsetHeight / 2) : 0;
   const width = nCont ? Math.floor(nCont.offsetWidth / 2) : 0;
@@ -26,11 +19,7 @@ export default function NumberSelector({clicked, setClicked, selectNumber, selPo
         <div className="selectorClose" onClick={() => selectNumber(clicked, 0)}>X</div>
         {
           [1,2,3,4,5,6,7,8,9].map( n => {
-            if (usedVals.includes(n)) {
-              return <div key={n}></div>
-            } else {
               return <div key={n} onClick={() => selectNumber(clicked, n)}>{n}</div>
-            }
           })
         }
       </div>
